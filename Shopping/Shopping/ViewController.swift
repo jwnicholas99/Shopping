8a28b2595d685a2ba1b9d08a71095c1ae4274319
Jing Wei Nicholas Lim <jingweinicholaslim@Jings-Mac.local>
2019-03-25 22:27:28 -0700

    first commit, practising

---
+++ Shopping/Shopping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by Jing Wei Nicholas Lim on 3/25/19.
+//  Copyright © 2019 Jing Wei Nicholas Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
